d2f216cccd44d9bd0a3e43b02b749638fa60b4a4
unknown <admin@MSI.tungvan.local>
2024-11-12 16:26:39 +0700

    done insert db

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,22 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRouter } from './boardRoute'
+import {columnRouter } from './columnRoute'
+import { cardRouter } from './cardRoute'
+
+
+const Router = express.Router()
+
+Router.get('/status', (req, res) => {
+    res.status(StatusCodes.OK).json({ message: 'api v1 ' })
+})
+
+Router.use('/boards', boardRouter)
+
+Router.use('/columns', columnRouter)
+
+Router.use('/cards', cardRouter)
+
+
+export const APIs_V1 = Router
--- src/server.js
+++ src/server.js
@@ -1,32 +1,40 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-console */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import cors from 'cors'
+import { corsOptions } from '~/config/cors'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import exitHook from 'async-exit-hook'
+import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1'
+import { errorHandlingMiddleware } from '~/middlewares/errorHanderMiddleware'
 
-const app = express()
+const START_SERVER = () => {
 
-const hostname = 'localhost'
-const port = 8017
+  const app = express()
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.use(cors(corsOptions))
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  app.use(express.json())
+
+  app.use('/v1', APIs_V1)
+
+  app.use(errorHandlingMiddleware)
+
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello ${env.AUTHOR}, I am running at ${env.APP_HOST}:${env.APP_PORT}/`)
+  })
+  exitHook(() => {
+    CLOSE_DB()
+  })
+}
+
+CONNECT_DB()
+  .then(() => console.log('Connected to MongoDB Cloud Atlas !'))
+  .then(() => START_SERVER())
+  .catch(error => {
+    console.error(error)
+    process.exit(0)
+  })
